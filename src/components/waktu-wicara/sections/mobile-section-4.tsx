62015b710364a609fc3aee494c046080ee852251
ahargunyllib <billy.bpm03@gmail.com>
2024-11-15 01:06:53 +0700

    feat: Implement scroll animations for MobileSection4

--- src/components/waktu-wicara/sections/mobile-section-4.tsx
+++ src/components/waktu-wicara/sections/mobile-section-4.tsx
@@ -1,8 +1,48 @@
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function MobileSection4() {
+  const { scrollYProgress } = useScroll();
+
+  const yPosition = useTransform(scrollYProgress, [0.3, 1], [0, -200]);
+
+  const tersembunyiOpacity = useTransform(
+    scrollYProgress,
+    [0.3, 0.335, 0.35],
+    [1, 0.5, 0],
+  );
+
+  const langkahOpacity = useTransform(
+    scrollYProgress,
+    [0.35, 0.4, 0.65, 0.7],
+    [0, 1, 1, 0],
+  );
+
+  const memanggilOpacity = useTransform(
+    scrollYProgress,
+    [0.7, 0.75, 1],
+    [0, 1, 1],
+  );
+
+  const imageIdx = useTransform(
+    scrollYProgress,
+    [0.3, 1],
+    [1, 8],
+  );
+  const [currentImageIdx, setCurrentImageIdx] = useState<number>(1);
+
+  useEffect(() => {
+    const unsubscribe = imageIdx.on("change", (latest) => {
+      setCurrentImageIdx(
+        Math.min(Math.floor(latest), 8),
+      );
+    });
+    return () => unsubscribe();
+  }, [imageIdx]);
+
   return (
-    <section className="relative h-dvh bg-[#080808] text-white">
+    <section className="relative h-[900vh] bg-[#080808] text-white">
       <div className="sticky left-0 top-0 h-screen w-full overflow-clip">
         <div className="absolute -left-56 top-0 aspect-video h-screen bg-[#080808]">
           <Image
@@ -25,8 +65,8 @@ export default function MobileSection4() {
 
         <div className="absolute -bottom-8 -left-52 aspect-square w-[42rem]">
           <Image
-            src="/img/hand-flower-1.png"
-            alt="Hand Flower 1"
+            src={`/img/hand-flower-${currentImageIdx}.png`}
+            alt={`Hand Flower ${currentImageIdx}`}
             fill
             priority
           />
@@ -36,15 +76,51 @@ export default function MobileSection4() {
           <div className="h-screen w-screen bg-gradient-to-t from-[#D9151C] to-transparent to-50% opacity-40" />
         </div>
 
-        <div className="absolute top-56 right-10 flex w-max flex-col items-center justify-center">
-          <h1 className="text-center font-header text-4xl leading-[4rem]">
+        <motion.div
+          className="absolute right-10 top-56 flex w-max flex-col items-start justify-center"
+          style={{
+            y: useTransform(yPosition, (value) => `${value}vh`),
+            opacity: tersembunyiOpacity,
+          }}
+        >
+          <h1 className="text-left font-header text-4xl leading-[4rem]">
             Tersembunyi
           </h1>
           <h2 className="w-56 text-justify font-light">
             Potensi dan mimpi bersemayam dalam diri, menanti saatnya terungkap
             dan menyuarakan apa yang selama ini terdiam.
           </h2>
-        </div>
+        </motion.div>
+
+        <motion.div
+          className="absolute right-10 top-56 flex w-max flex-col items-start justify-center"
+          style={{
+            y: useTransform(yPosition, (value) => `${value + 100}vh`),
+            opacity: langkahOpacity,
+          }}
+        >
+          <h1 className="text-left font-header text-4xl leading-[4rem]">
+            Langkah
+          </h1>
+          <h2 className="w-56 text-justify font-light">
+            Setelah menyentuh potensi tersembunyi, keberanian lahir, mengajak
+            kaki melangkah keluar dari batas nyaman.
+          </h2>
+        </motion.div>
+
+        <motion.div
+          className="absolute right-10 top-56 flex w-max flex-col items-start justify-center"
+          style={{
+            y: useTransform(yPosition, (value) => `${value + 200}vh`),
+            opacity: memanggilOpacity,
+          }}
+        >
+          <h1 className="font-header text-4xl leading-[4rem]">Memanggil</h1>
+          <h2 className="w-56 text-justify font-light">
+            Ada panggilan harlus dari dalam jiwa atau dari semesta, menggugah
+            hati untuk melangkah lebih jauh menjemput takdir
+          </h2>
+        </motion.div>
       </div>
     </section>
   );
