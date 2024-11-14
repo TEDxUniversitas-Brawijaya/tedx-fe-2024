cdf4e312fc89567ae9d8c838f7c54ee850481ae3
ahargunyllib <billy.bpm03@gmail.com>
2024-11-14 22:13:51 +0700

    feat: Add dummy MobileSection3 component to WaktuWicaraPageMobile

--- src/components/waktu-wicara/screens/mobile.tsx
+++ src/components/waktu-wicara/screens/mobile.tsx
@@ -1,11 +1,13 @@
 import MobileSection1 from "../sections/mobile-section-1";
 import MobileSection2 from "../sections/mobile-section-2";
+import MobileSection3 from "../sections/mobile-section-3";
 
 export default function WaktuWicaraPageMobile() {
   return (
     <main>
       <MobileSection1 />
       <MobileSection2 />
+      <MobileSection3 />
     </main>
   );
 }
---
+++ src/components/waktu-wicara/sections/mobile-section-3.tsx
@@ -0,0 +1,9 @@
+export default function MobileSection3() {
+  return (
+    <section className="flex h-screen items-center justify-center bg-black">
+      <div className="flex aspect-video h-3/4 w-3/4 items-center justify-center bg-gray-500 text-white">
+        <p>Video goes here</p>
+      </div>
+    </section>
+  );
+}
