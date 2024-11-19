a7082a507814daa0ecf2a96d4f8acca0d926816a
ahargunyllib <billy.bpm03@gmail.com>
2024-11-19 21:25:11 +0700

    feat: add awan between sec2 and sec3

--- src/components/swara-aksara/screens/desktop.tsx
+++ src/components/swara-aksara/screens/desktop.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import DesktopSection1 from "../sections/desktop-section-1";
 import DesktopSection2 from "../sections/desktop-section-2";
 import DesktopSection3 from "../sections/desktop-section-3";
@@ -6,7 +7,14 @@ export default function SwaraAksaraPageDesktop() {
   return (
     <main>
       <DesktopSection1 />
-      <DesktopSection2 />
+      <div>
+        <DesktopSection2 />
+        <div className="sticky z-50 flex items-center justify-center overflow-x-clip">
+          <div className="absolute aspect-[4/1] h-fit w-[102.5vw]">
+            <Image src={"/img/awan.png"} alt="Awan" fill />
+          </div>
+        </div>
+      </div>
       <DesktopSection3 />
     </main>
   );
