aa63816b22a203043f414ef82cd289748813aa65
ahargunyllib <billy.bpm03@gmail.com>
2024-12-15 17:56:08 +0700

    fix: update MobileSection3 height to full screen

--- src/components/api-kehadiran/sections/mobile-section-3.tsx
+++ src/components/api-kehadiran/sections/mobile-section-3.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function MobileSection3() {
   return (
-    <section className="relative z-0 h-[75vh] overflow-hidden bg-[#1A1A1A]">
-      <div className="sticky left-0 top-0 flex h-[75vh] w-full items-center justify-center">
+    <section className="relative z-0 h-screen overflow-hidden bg-[#1A1A1A]">
+      <div className="sticky left-0 top-0 flex h-screen w-full items-center justify-center">
         <div className="absolute h-full w-full bg-gradient-to-t from-[#131012] to-[#540D00]" />
 
         <div className="absolute aspect-video h-[130%] opacity-25">
