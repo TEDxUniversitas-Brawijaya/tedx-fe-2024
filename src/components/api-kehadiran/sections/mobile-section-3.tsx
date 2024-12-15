d4bd43993e40d35fa25831743c58573a17a8cda0
ahargunyllib <billy.bpm03@gmail.com>
2024-12-15 16:17:20 +0700

    feat: add MobileSection3

---
+++ src/components/api-kehadiran/sections/mobile-section-3.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default function MobileSection3() {
+  return (
+    <section className="relative z-0 h-[75vh] overflow-hidden bg-[#1A1A1A]">
+      <div className="sticky left-0 top-0 flex h-[75vh] w-full items-center justify-center">
+        <div className="absolute h-full w-full bg-gradient-to-t from-[#131012] to-[#540D00]" />
+
+        <div className="absolute aspect-video h-[130%] opacity-25">
+          <Image
+            src={"/img/paper-texture-6.png"}
+            alt="Paper Texture"
+            fill
+            priority
+          />
+        </div>
+
+        <div className="absolute flex flex-col text-center items-center justify-center gap-11 text-white w-[24rem]">
+          <div className="flex w-[22rem] flex-col items-center justify-center gap-6">
+            <h1 className="font-header text-2xl">
+              "Langkah Kecil Adalah Kunci Untuk Menemukan Terang"
+            </h1>
+            <div className="h-[12rem] w-full bg-white" />
+          </div>
+          <p className="text-xs">
+            Dalam pemberhentian singkat ini, kami ingin membagikan beberapa
+            pengalaman teman-teman dalam proses kembali menemukan api yang telah
+            redup. Jadikan ini sebagai langkah kecil kamu untuk menjadi bagian
+            dari perubahan
+          </p>
+        </div>
+
+        <div className="absolute top-0 h-[35%] w-full bg-gradient-to-b from-[#0E0E0E] from-5% to-transparent to-80%" />
+      </div>
+    </section>
+  );
+}
