6fcc780ab20974f336f3778a257d843839b890a8
LeonardTarigan <leontrg32@gmail.com>
2024-12-18 04:56:33 +0700

    feat(propa-2): separating desktop glass fragments into 3 subsets

--- src/components/api-kehadiran/sections/desktop-section-1.tsx
+++ src/components/api-kehadiran/sections/desktop-section-1.tsx
@@ -1,60 +1,79 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
-import { motion, useSpring } from "framer-motion";
-import { useEffect, useState } from "react";
 
 const DesktopSection1 = () => {
-  const [targetPosition, setTargetPosition] = useState({ x: 0, y: 0 });
-
-  const springX = useSpring(0, { stiffness: 50, damping: 10 });
-  const springY = useSpring(0, { stiffness: 50, damping: 10 });
-
-  useEffect(() => {
-    springX.set(targetPosition.x);
-    springY.set(targetPosition.y);
-  }, [targetPosition, springX, springY]);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const cardRect = e.currentTarget.getBoundingClientRect();
-    const centerX = cardRect.left + cardRect.width / 2;
-    const centerY = cardRect.top + cardRect.height / 2;
-
-    const offsetX = e.clientX - centerX;
-    const offsetY = e.clientY - centerY;
-
-    const maxMovement = 6;
-    const distance = Math.sqrt(offsetX ** 2 + offsetY ** 2);
-    if (distance > maxMovement) {
-      const angle = Math.atan2(offsetY, offsetX);
-
-      setTargetPosition({
-        x: -Math.cos(angle) * maxMovement,
-        y: -Math.sin(angle) * maxMovement,
-      });
-    } else {
-      setTargetPosition({
-        x: -offsetX / 3,
-        y: -offsetY / 3,
-      });
-    }
-  };
-
   return (
-    <section
-      className="relative flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden text-white"
-      onMouseMove={handleMouseMove}
-    >
-      <h1 className="z-20 text-3xl">Propaganda 1</h1>
+    <section className="relative flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden text-white">
+      <h1 className="z-20 text-3xl">Propaganda 2</h1>
       <h2 className="z-20 font-header text-8xl">Api Kehadiran</h2>
 
       <motion.div
         className="absolute left-0 top-0 z-10 h-screen w-full"
-        style={{
-          x: springX,
-          y: springY,
+        initial={{ y: 0 }}
+        animate={{ y: -20 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 1.75,
+          ease: "easeInOut",
+          bounce: 0.5,
+          stiffness: 100,
+          mass: 1,
+          delay: 0,
         }}
       >
         <Image
-          src="/img/glass-desktop.png"
+          src="/img/flame-fragments-desktop-1.png"
+          alt="Glass"
+          className="object-cover"
+          draggable={false}
+          fill
+          priority
+        />
+      </motion.div>
+
+      <motion.div
+        className="absolute left-0 top-0 z-10 h-screen w-full"
+        initial={{ y: 0 }}
+        animate={{ y: -20 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 1.75,
+          ease: "easeInOut",
+          bounce: 0.5,
+          stiffness: 100,
+          mass: 1,
+          delay: 0.3,
+        }}
+      >
+        <Image
+          src="/img/flame-fragments-desktop-2.png"
+          alt="Glass"
+          className="object-cover"
+          draggable={false}
+          fill
+          priority
+        />
+      </motion.div>
+
+      <motion.div
+        className="absolute left-0 top-0 z-10 h-screen w-full"
+        initial={{ y: 0 }}
+        animate={{ y: -20 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 1.75,
+          ease: "easeInOut",
+          bounce: 0.5,
+          stiffness: 100,
+          mass: 1,
+          delay: 0.6,
+        }}
+      >
+        <Image
+          src="/img/flame-fragments-desktop-3.png"
           alt="Glass"
           className="object-cover"
           draggable={false}
