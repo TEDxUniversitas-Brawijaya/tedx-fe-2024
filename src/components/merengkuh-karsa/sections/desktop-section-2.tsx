b8cf9331ad1db23373078fb4c430127e401f16d4
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2025-01-09 19:51:14 +0700

    feat: add video placeholder in `desktop-section-2`

--- src/components/merengkuh-karsa/sections/desktop-section-2.tsx
+++ src/components/merengkuh-karsa/sections/desktop-section-2.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import Image from "next/image";
 import RevealLetterDesktop from "../ui/reveal-letter-desktop";
 
@@ -9,7 +9,7 @@ const DesktopSection2 = () => {
 
   const sectionOpacity = useTransform(
     scrollYProgress,
-    [0, 0.2, 0.8],
+    [0, 0.2, 0.4],
     [0.4, 1, 1],
   );
 
@@ -20,15 +20,20 @@ const DesktopSection2 = () => {
   const totalChars = text.length;
   let charCount = 0;
 
+  const videoX = useSpring(
+    useTransform(scrollYProgress, [0.5, 0.6], [1000, 0]),
+    { stiffness: 100, damping: 50 },
+  );
+
   return (
     <section className="relative h-[400vh] w-full bg-black">
       <motion.div
-        className="sticky left-0 top-0 h-screen w-full"
+        className="sticky left-0 top-0 h-screen w-full overflow-hidden"
         style={{ opacity: sectionOpacity }}
       >
         <div className="absolute left-10 top-1/2 z-10 -translate-y-1/2 space-y-2 text-white">
           <h2 className="font-header text-5xl font-semibold">Membara</h2>
-          <p className="w-[60%] text-lg">
+          <p className="w-[45%] text-lg">
             {words.map((word, wordIndex) => (
               <span key={wordIndex} className="inline-block whitespace-nowrap">
                 {word.split("").map((char) => (
@@ -54,6 +59,12 @@ const DesktopSection2 = () => {
             ))}
           </p>
         </div>
+        <motion.div
+          style={{
+            x: videoX,
+          }}
+          className="absolute right-10 top-1/4 z-10 aspect-video h-auto w-[45%] -translate-x-1/2 bg-white drop-shadow-[0px_4px_70px_rgba(255,0,0,0.5)]"
+        />
         <div className="absolute bottom-0 left-0 h-[calc(100dvh-62px)] w-full opacity-60">
           <Image
             src="https://res.cloudinary.com/dcvnwpyd9/image/upload/v1736337572/tedxuniversitasbrawijaya2025/heartbeat-with-b6mbg_ncltji.gif"
