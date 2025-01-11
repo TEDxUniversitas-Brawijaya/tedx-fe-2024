90ac17c5f58756c6368badd61e0d1dd05500b774
LeonardTarigan <leontrg32@gmail.com>
2025-01-11 23:59:53 +0700

    feat: propa 3 countdown

--- src/components/merengkuh-karsa/sections/mobile-section-3.tsx
+++ src/components/merengkuh-karsa/sections/mobile-section-3.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import CountdownTimer from "../ui/countdown-timer";
 
 export default function MobileSection3() {
   return (
     <>
       <div className="h-20 w-full bg-gradient-to-b from-[#100D10] to-[#0E0E0E]"></div>
-      <section className="relative flex h-[90dvh] w-full items-center justify-center bg-[#0E0E0E]">
+      <section className="relative flex h-[90dvh] w-full items-center justify-center bg-[#0E0E0E] text-white">
         <div className="absolute bottom-0 h-1/2 w-full opacity-50">
           <Image
             src="/img/walk-door-desktop.png"
@@ -16,14 +17,19 @@ export default function MobileSection3() {
           />
         </div>
 
-        <div className="absolute mb-14 flex w-[70%] flex-col items-center justify-center gap-y-9 text-center">
-          <p className="w-80 text-lg font-semibold text-white">
+        <div className="absolute mb-14 flex flex-col items-center justify-center gap-y-20 px-5 text-center">
+          <p className="text-lg font-semibold sm:w-[70%]">
             Perjalanan menuju Mantra Diri masih berlanjut, teruslah menyelami
             makna dengan karsa yang direngkuh.
           </p>
-          <button className="rounded-md bg-tedx-red px-7 py-3 font-body text-base font-semibold text-white transition-all duration-150 hover:bg-tedx-red/80 disabled:opacity-50">
-            Daftarkan Dirimu
-          </button>
+          <div className="flex flex-col items-center">
+            <p>Babak ini akan dimulai dalam</p>
+            <CountdownTimer />
+            <p className="max-w-[400px]">
+              Pantau terus linimasa kami untuk mengetahui informasi lebih lanjut
+              tentang Propaganda 3!
+            </p>
+          </div>
         </div>
       </section>
     </>
---
+++ src/components/merengkuh-karsa/ui/countdown-timer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useCountdown } from "@/hooks/useCountdown";
+import useMounted from "@/hooks/useMounted";
+
+export default function CountdownTimer() {
+  const mounted = useMounted();
+
+  const { days, hours, minutes, seconds } = useCountdown(
+    new Date(2025, 0, 19, 10, 0, 0),
+  );
+
+  if (!mounted)
+    return (
+      <h3 className="font-header text-9xl font-bold text-tedx-red">
+        --:--:--:--
+      </h3>
+    );
+
+  return (
+    <h3 className="font-header text-5xl font-bold text-tedx-red lg:text-9xl">
+      {days}:{hours}:{minutes}:{seconds}
+    </h3>
+  );
+}
