71361ab8bbfbe4b6de195e8a36e7076687319f48
ahargunyllib <billy.bpm03@gmail.com>
2025-01-21 01:41:05 +0700

    feat(merch): add Footer component to MerchPage

--- src/app/(landing)/store/merch/page.tsx
+++ src/app/(landing)/store/merch/page.tsx
@@ -1,11 +1,13 @@
-import Section2 from "@/components/store/merch/sections/section-2";
+import Footer from "@/components/shared/footer";
 import Section1 from "@/components/store/merch/sections/section-1";
+import Section2 from "@/components/store/merch/sections/section-2";
 
 export default function MerchPage() {
   return (
     <main>
       <Section1 />
       <Section2 />
+      <Footer />
     </main>
   );
 }
